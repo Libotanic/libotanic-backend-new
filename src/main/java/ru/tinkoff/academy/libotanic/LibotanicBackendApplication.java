4026d83610e0c6fcd263b95c1e7b77dd6865549e
ag.shevchenko <ag.shevchenko@tinkoff.ru>
2023-03-23 22:27:28 +0300

    application config

--- src/main/java/ru/tinkoff/academy/libotanic/LibotanicBackendApplication.java
+++ src/main/java/ru/tinkoff/academy/libotanic/LibotanicBackendApplication.java
@@ -2,10 +2,14 @@ package ru.tinkoff.academy.libotanic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+
 
 @SpringBootApplication
 public class LibotanicBackendApplication {
-
+  @Profile(value = "prod")
   public static void main(String[] args) {
     SpringApplication.run(LibotanicBackendApplication.class, args);
   }
