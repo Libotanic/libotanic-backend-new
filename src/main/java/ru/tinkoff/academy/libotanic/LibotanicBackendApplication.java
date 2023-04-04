e302b3c4dd5b4a8f5ded926676737d795735de08
agatiks <agatikc@gmail.com>
2023-04-04 21:08:14 +0300

    added swagger

--- src/main/java/ru/tinkoff/academy/libotanic/LibotanicBackendApplication.java
+++ src/main/java/ru/tinkoff/academy/libotanic/LibotanicBackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
-
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
