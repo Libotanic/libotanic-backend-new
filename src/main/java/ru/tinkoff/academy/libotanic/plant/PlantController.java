6e656db2691a8930bb647184d21fe8a0436d225a
agatiks <agatikc@gmail.com>
2023-04-06 07:35:28 +0300

    fix get plant by id

--- src/main/java/ru/tinkoff/academy/libotanic/plant/PlantController.java
+++ src/main/java/ru/tinkoff/academy/libotanic/plant/PlantController.java
@@ -1,18 +1,29 @@
 package ru.tinkoff.academy.libotanic.plant;
 
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.tinkoff.academy.libotanic.abstract_management.AbstractController;
 import ru.tinkoff.academy.libotanic.abstract_management.AbstractService;
 import ru.tinkoff.academy.libotanic.domain.plant.Plant;
+import ru.tinkoff.academy.libotanic.domain.plant.PlantDto;
+import ru.tinkoff.academy.libotanic.domain.plant.PlantMapper;
 
 @RestController
 @RequestMapping("/plants")
+@RequiredArgsConstructor
+public class PlantController {
+  protected final AbstractService<Plant> service;
+  protected final PlantMapper plantMapper;
 
-public class PlantController extends AbstractController<Plant> {
-
-  public PlantController(
-      AbstractService<Plant> service) {
-    super(service);
+  @GetMapping("/{id}")
+  public ResponseEntity<PlantDto> findById(@PathVariable Long id) {
+    Optional<Plant> optionalEntity = service.findById(id);
+    return optionalEntity.map(value -> new ResponseEntity<>(plantMapper.toDto(value), HttpStatus.OK))
+        .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
   }
 }
